a83a4385b83ba5679efa328fd7388e9c7e8f1ce7
C2P <office@code2prog.com>
2023-03-07 12:22:55 +0200

    comment funkywhite space

--- src/WhitespacePathNormalizer.php
+++ src/WhitespacePathNormalizer.php
@@ -9,16 +9,16 @@ class WhitespacePathNormalizer implements PathNormalizer
     public function normalizePath(string $path): string
     {
         $path = str_replace('\\', '/', $path);
-        $this->rejectFunkyWhiteSpace($path);
+        //$this->rejectFunkyWhiteSpace($path);
 
         return $this->normalizeRelativePath($path);
     }
 
     private function rejectFunkyWhiteSpace(string $path): void
     {
-//        if (preg_match('#\p{C}+#u', $path)) {
-//            throw CorruptedPathDetected::forPath($path);
-//        }
+        if (preg_match('#\p{C}+#u', $path)) {
+            throw CorruptedPathDetected::forPath($path);
+        }
     }
 
     private function normalizeRelativePath(string $path): string
